225cbcf7158996e4e66c1aed7e73e989c73fe797
weijie <qoo77823@hotmail.com>
2025-12-05 20:51:55 +0800

    feat: add user.repository.interface.ts

---
+++ src/repositories/user.repository.interface.ts
@@ -0,0 +1,11 @@
+import { User } from "../models/user.model"
+
+export interface IUserRepository {
+  createUser(user: Omit<User, "id">): Promise<User>;
+  findByUsername(username: string): Promise<User | null>;
+}
+
+/*
+Omit<User, "id">:
+忽視或刪除 User type 的 id 的 key 值，如果沒有在 User type 定義 id 則沒有作用
+*/
