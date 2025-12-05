5158239b26eb179b9e65042ffa29b7167b8e5d6c
weijie <qoo77823@hotmail.com>
2025-12-06 01:55:30 +0800

    feat: add user.controller.ts

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,26 @@
+import { UserService } from "../services/user.services";
+import { Request, Response } from "express";
+
+export class UserController {
+  constructor(private userServices: UserService) {}
+
+  register = async (req: Request, res: Response) => {
+    try {
+      const user = await this.userServices.register(req.body);
+      res.json({ message: "Registered", user });
+    } catch (err: unknown) {
+      if (err instanceof Error) res.status(400).json({ error: err.message });
+      return res.status(400).json({ error: "Unknown error" });
+    }
+  };
+
+  login = async (req: Request, res: Response) => {
+    try {
+      const user = await this.userServices.login(req.body);
+      res.json({ message: "Login success!", user });
+    } catch (err: unknown) {
+      if (err instanceof Error) res.status(400).json({ error: err.message });
+      return res.status(400).json({ error: "Unknown error" });
+    }
+  };
+}
