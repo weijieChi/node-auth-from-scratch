57488b55d5bfc1df4718717d8c0a490b5b971deb
weijie <qoo77823@hotmail.com>
2025-12-14 02:08:16 +0800

    feat: change Morgan message format to JSON.

--- src/logger/morgan.middleware.ts
+++ src/logger/morgan.middleware.ts
@@ -3,8 +3,20 @@ import { logger } from "./winston.logger.js";
 
 const stream = {
   write: (message: string) => {
-    logger.info(message.trim()); // 寫入 winston
+    logger.info(JSON.parse(message)); // 寫入 winston
   },
 };
+morgan.format("http-json", (tokens, req, res) => {
+  return JSON.stringify({
+    type: "http", // 用於表示 log 的來源
+    remoteAddr: tokens['remote-addr']?.(req, res) ?? "-",
+    method: tokens.method?.(req, res) ?? "-", // ?. ??
+    url: tokens.url?.(req, res) ?? "-",
+    status: Number(tokens.status?.(req, res) ?? 0),
+    contentLength: tokens.res?.(req, res, 'content-length') ?? "-",
+    responseTimeMs: Number(tokens["response-time"]?.(req, res) ?? 0),
+    timestamp: new Date().toISOString(),
+  });
+});
 
-export const httpLogger = morgan("combined", { stream });
+export const httpLogger = morgan("http-json", { stream });
