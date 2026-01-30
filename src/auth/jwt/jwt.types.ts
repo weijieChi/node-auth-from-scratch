4436f23df7418898912e696a05732f3324971499
weijie <qoo77823@hotmail.com>
2026-01-31 00:50:44 +0800

    fix: correcting english capitalization

--- src/auth/jwt/jwt.types.ts
+++ src/auth/jwt/jwt.types.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 // access token
-export const AccessTokenPayLoadSchema = z.object({
+export const AccessTokenPayloadSchema = z.object({
   sub: z.number(), // user id
   type: z.literal("ACCESS"),
   iat: z.number(), // issued at (UNIX timestamp)
   exp: z.number(), // expires at (UNIX timestamp)
 });
 
-export type AccessTokenPayload = z.infer<typeof AccessTokenPayLoadSchema>;
+export type AccessTokenPayload = z.infer<typeof AccessTokenPayloadSchema>;
 
 // refresh token
 export const RefreshTokenPayloadSchema = z.object({
@@ -22,9 +22,14 @@ export const RefreshTokenPayloadSchema = z.object({
 
 export type RefreshTokenPayload = z.infer<typeof RefreshTokenPayloadSchema>;
 
+// 意思是：
+// 如果 type === "ACCESS"
+// payload 一定是 AccessTokenPayload
+// 如果 type === "REFRESH"
+// payload 一定是 RefreshTokenPayload
 // 這不是必須，但會讓 verify flow 很乾淨。
 export const JwtPayloadSchema = z.discriminatedUnion("type", [
-  AccessTokenPayLoadSchema,
+  AccessTokenPayloadSchema,
   RefreshTokenPayloadSchema,
 ]);
 
